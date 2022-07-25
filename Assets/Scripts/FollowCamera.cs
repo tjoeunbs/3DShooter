f0148e8d17b92d8c81fd9fb6937812a846595a09
tjoeunbs <tjoeunbs@naver.com>
2022-07-25 11:21:32 +0900

    2교시

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,9 +13,17 @@ public class Player : MonoBehaviour
 
     Animator anim;
 
+    bool walkKeyDown;
+    bool jumpKeyDown;
+
+    Rigidbody rigid;
+
+    bool isJump;
+
     private void Awake()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
     // Start is called before the first frame update
     void Start()
@@ -28,14 +36,41 @@ public class Player : MonoBehaviour
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        walkKeyDown = Input.GetButton("Walk");
+        jumpKeyDown = Input.GetButtonDown("Jump");
 
         Move();
+        Jump();
     }
 
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * speed * Time.deltaTime;
+        transform.position += moveVec * speed * (walkKeyDown ? 0.3f : 1f) * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", walkKeyDown);
+
+        transform.LookAt(transform.position + moveVec); // 앞으로 가는 방향으로 로테이션
+    }
+
+    void Jump()
+    {
+        if(jumpKeyDown)
+        {
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+        }
     }
 }
